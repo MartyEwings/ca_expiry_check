c1cc0efa9fc78e39ed17cd6ec4df9164518f46d7
Adrian <sensei.loafage@gmail.com>
2020-05-01 09:36:56 -0700

    Use cut instead of ERE

--- lib/facter/ca_exp_date.rb
+++ lib/facter/ca_exp_date.rb
@@ -3,6 +3,6 @@ Facter.add(:ca_exp_date) do
     File.exist? '/etc/puppetlabs/puppet/ssl/certs/ca.pem'
   end
   setcode do
-    Facter::Core::Execution.execute('/opt/puppetlabs/puppet/bin/openssl x509 -enddate -noout -in /etc/puppetlabs/puppet/ssl/certs/ca.pem | sed -r \'s/.{9}//\'')
+    Facter::Core::Execution.execute('/opt/puppetlabs/puppet/bin/openssl x509 -enddate -noout -in /etc/puppetlabs/puppet/ssl/certs/ca.pem | cut -f2- -d "="')
   end
 end
