83739dad92f742e3ab6103a2267a75e12c0020b3
martyewings <martyewings@gmail.com>
2021-05-04 14:31:46 +0100

    puppet 7 CA Dir considerations

--- lib/facter/ca_exp_seconds.rb
+++ lib/facter/ca_exp_seconds.rb
@@ -2,12 +2,15 @@ require 'openssl'
 require 'time'
 
 Facter.add(:ca_exp_seconds) do
-  ca_file = '/etc/puppetlabs/puppet/ssl/ca/ca_crt.pem'
   confine do
-    File.exist? ca_file
+    File.exist? '/etc/puppetlabs/puppet/ssl/ca/ca_crt.pem' or File.exist? '/etc/puppetlabs/puppetserver/ca/ca_crt.pem'
   end
   setcode do
-    raw_ca_cert = File.read ca_file
+    raw_ca_cert = if File.exist? '/etc/puppetlabs/puppetserver/ca/ca_crt.pem'
+                    File.read '/etc/puppetlabs/puppetserver/ca/ca_crt.pem'
+                  else
+                    File.read '/etc/puppetlabs/puppet/ssl/ca/ca_crt.pem'
+                  end
     certificate = OpenSSL::X509::Certificate.new raw_ca_cert
     certificate.not_after - Time.now
   end
